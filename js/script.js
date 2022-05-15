b7745bae920ce769c291f128f8e44fad30cd8d52
Joseph Barbone <JoeBarbone@aol.com>
2022-05-14 22:19:59 -0400

    UX enhancements

--- js/script.js
+++ js/script.js
@@ -183,7 +183,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -211,13 +211,19 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("over", event.target);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    //console.log("out", event.target);
   },
   update: function(event) {
     // loop over current set of children in sortable list
@@ -261,12 +267,13 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $("#trash .bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $("#trash .bottom-trash").removeClass("bottom-trash-active");
   }
 
 })
@@ -291,7 +298,7 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
   // this should print out an object for the value of the date variable but at 5:00pm of that date
-  // console.log(time);
+  console.log(taskEl);
   
 }
 
@@ -307,7 +314,13 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+setInterval(function() {
+  // I don't get the index, el part here get help with this
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
 // load tasks for the first time
 loadTasks();
-
 
